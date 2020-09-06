4a1b9f2d42da504702b6e2c5f54528237f6dff90
Isamu Takemoto <takeisa0201@gmail.com>
2020-09-06 11:36:21 +0900

    Updates

---
+++ precache-manifest.48c5b44d54edb2aac0735b478405700e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a436153026bbfb5fae413198755ec6",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "6316210d4cfc182fe388",
+    "url": "/react-portfolio/static/css/main.33e0e534.chunk.css"
+  },
+  {
+    "revision": "33921cb028d954044c53",
+    "url": "/react-portfolio/static/js/2.76f48caf.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/react-portfolio/static/js/2.76f48caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6316210d4cfc182fe388",
+    "url": "/react-portfolio/static/js/main.5cccf91d.chunk.js"
+  },
+  {
+    "revision": "383ae605a22a8d571f51",
+    "url": "/react-portfolio/static/js/runtime-main.9a42905e.js"
+  }
+]);
